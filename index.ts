5539683e2895e158433d1f4170dfe6de717cc1f1
Ribó <elribonazo@gmail.com>
2021-01-18 20:30:51 +0100

    publish upgrade 0.0.6 with new interface for controllers

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export * as Compiler from './lib/compiler/index';
 export * as Server from './lib/server';
 export * as ServerAnnotations from './lib/server/annotations';
-export * as Cli from './lib/cli'
+export * as Cli from './lib/cli';
+export * as I from './lib/interfaces';
 
